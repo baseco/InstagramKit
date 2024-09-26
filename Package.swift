2462e14cd0ba0b535c2c39c224bffc1ddff6e213
StefanChannel <stefan@channel.surf>
2024-09-26 14:07:14 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/baseco/AFNetworking.git", .branch("2.3.1_with_package_swift")),
+        .package(url: "https://github.com/baseco/AFNetworking.git", .revision("75441a7")),
         // .package(url: "https://github.com/kishikawakatsumi/UICKeyChainStore.git", from: "2.2.1")
     ],
     targets: [
